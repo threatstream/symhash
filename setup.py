e0483c38b3beb628184e9aee62f1ca4d76df90de
Marcus LaFerrera <marcus@randomhack.org>
2016-09-28 09:54:57 -0400

    Added support for python3

--- setup.py
+++ setup.py
@@ -3,27 +3,28 @@
 from setuptools import setup
 
 setup(
-    name = 'symhash',
-    version = '0.0.1',
-    url = 'https://github.com/threatstream/symhash',
-    author = 'Aaron Shelmire',
-    author_email = 'aaron.shelmire@anomali.com',
-    license = 'GNU GPLv3',
-    packages = [
+    name='symhash',
+    version='0.0.2',
+    url='https://github.com/threatstream/symhash',
+    author='Aaron Shelmire',
+    author_email='aaron.shelmire@anomali.com',
+    license='GNU GPLv3',
+    packages=[
         'symhash',
     ],
     scripts=[
         'bin/symhash'
     ],
-    install_requires = [
+    install_requires=[
         'filemagic==1.6',
+        'future==0.15.2',
     ],
-    description = 'Anomali Symhash',
+    description='Anomali Symhash',
     classifiers=[
         'License :: OSI Approved :: GNU General Public License v2 (GPLv2)',
         'Operating System :: POSIX',
         'Programming Language :: Other Scripting Engines',
-        'Programming Language :: Python :: 2 :: Only',
+        'Programming Language :: Python :: 3',
         'Programming Language :: Python :: 2',
         'Programming Language :: Python :: 2.7',
     ]
--- symhash/__init__.py
+++ symhash/__init__.py
@@ -15,47 +15,56 @@
 import magic
 import logging
 from hashlib import md5
+
 log = logging.getLogger(__name__)
 
+try:
+    from symhash.machoinfo import MachOEntity, MachOParser, MachOParserError
+except:
+    log.error("machoinfo is required - if installation did not work, retreive"
+              "machoinfo.py from https://raw.githubusercontent.com/crits/crits_services/master/machoinfo_service/machoinfo.py")
+    exit(-1)
+
+
 def create_sym_hash(filename):
-    ''' create the sym hash '''
+    # create the sym hash
     with open(filename, 'rb') as f:
-        filedata = open(filename, 'rb').read()
-        f.close()
+        filedata = f.read()
+
     with magic.Magic() as m:
         filetype = m.id_filename(filename)
 
     if 'Mach-O' not in filetype:
-        log.error('symhash only operates on Mach-O Executable files')
+        log.error("symhash only operates on Mach-O Executable files")
+
+    macho_parser = MachOParser(filedata)
 
     try:
-        from machoinfo import MachOEntity, MachOParser, MachOParserError
-    except:
-        log.error('machoinfo is required - if installation did not work, retreive machoinfo.py from https://raw.githubusercontent.com/crits/crits_services/master/machoinfo_service/machoinfo.py')
-    macho_parser = MachOParser(filedata)
-    try:
         macho_parser.parse()
-    except MachOParserError, e:
+    except MachOParserError as e:
         log.error("Error %s", e)
         return None
+
     sym_dict = {}
+
     for entity in macho_parser.entities:
         if entity.magic_str != 'Universal':
 
-            entity_string = '%s %s %s' % (
-                    entity.cpu_type_str,
-                    entity.filetype_str,
-                    entity.magic_str)
+            entity_string = "{} {} {}".format(entity.cpu_type_str,
+                                              entity.filetype_str,
+                                              entity.magic_str)
+
             sym_list = []
+
             for cmd in entity.cmdlist:
                 if cmd['cmd'] == MachOEntity.LC_SYMTAB:
                     for sym in cmd['symbols']:
                         if not sym['is_stab']:
                             if sym['external'] is True:
                                 if sym['n_type'] == '0x00':
-                                    sym_list.append(sym.get('string', ''))
+                                    sym_list.append(sym.get('string', '').decode())
 
-            symhash = md5(','.join(sorted(sym_list))).hexdigest()
+            symhash = md5(','.join(sorted(sym_list)).encode()).hexdigest()
             sym_dict[entity_string] = symhash
 
     return sym_dict
